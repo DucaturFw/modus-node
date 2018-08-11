5c97a20d411a14907c7a14c64656af2bafb41247
Vitaly Berg <bergusman@gmail.com>
2018-08-12 01:59:43 +0530

    Change config

--- chain-main.js
+++ chain-main.js
@@ -1,17 +1,7 @@
 const Ganache = require('ganache-core')
 const BN = require("bn.js")
 
-const options = {
-    port: 3416,
-    hostname: '0.0.0.0',
-    db_path: '../gn_main',
-    accounts: [
-        {
-            secretKey: '0xebed62445f346abe7f51bd1a80be9788007570a83e9c43a5a45cb49c33b9a85f', 
-            balance: '10000000000000000000000'
-        }
-    ]
-}
+const options = require('./config').nets.main
 
 const server = Ganache.server(options)
 server.listen(options.port, options.hostname, (err, bc) => {
